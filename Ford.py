65e4210d93523bb6ea80a9da6f209793c8991ed5
erick <erikc_48@hotmail.com>
2023-03-14 08:01:17 -0600

    Update

--- Ford.py
+++ Ford.py
@@ -1,43 +1,48 @@
-import pandas as pd
 import streamlit as st
-import base64
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
 
-st.title('Ford Used Car Listing')
+#--- IMPORT DATA ---#
+df=pd.read_csv("dataset/ford.csv")
 
-DATA_URL=('dataset/ford.csv')
+#--- PAGE CONFIG ---#
+st.set_page_config(page_title="Ford Used Car Listing",
+                    page_icon=":busts_in_silhouette:")
 
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+st.title("Ford Used Car Listing")
+st.write("Autor: Erick Juarez Espinosa")
+st.write("Matricula: S20006728")
+st.header("Descripcion del sitio")
+st.markdown("Este sitio sirve para poder listar los autos de la marca ford con su año de fabricacion y su precio hasta la actualidad")
 
-@st.cache
+#--- LOGO ---#
+st.sidebar.image("img/logo.png")
+st.sidebar.markdown("##")
+
+#--- FUNCIONES ---#
+@st.cache_data
 def load_data(nrows):
-    data = pd.read_csv(DATA_URL)
+    data = df 
     return data
 
 def load_data_byname(name):
-    data = pd.read_csv(DATA_URL)
-    filtered_data_byname = data[data['model'].str.contains(name)]
+    data = df
+    filtered_data_byname = data[data['model'].str.contains(name, case = False)]
     return filtered_data_byname
 
 def load_data_bytransmission(transmission):
-    data = pd.read_csv(DATA_URL)
+    data = df
     filtered_data_bytras = data[data[ 'transmission' ] == transmission]
 
     return filtered_data_bytras
 
-st.header('Erick Juaerz Espinosa')
-st.header('ZS20006728')
-
-data_load_state = st.text('Data cargada')
-data = load_data(500)
 st.header("Todos los carros")
+data_load_state = st.text('Data cargada . . .')
+data = load_data(100)
 
-st.sidebar.image("img/logo.png")
-st.sidebar.markdown("##")
-
+#--- SIDEBAR FILTERS ---#
 sidebar = st.sidebar
 agree = sidebar.checkbox("Mostrar todos los Modelos de auntos")
 if agree:
@@ -50,7 +55,7 @@ if (myname):
     if (btnRange):
         filterbyname = load_data_byname(myname)
         count_row = filterbyname.shape[0]
-        st.write(f"Buscar autos : {count_row}")
+        st.write(f"Total de autos : {count_row}")
         st.dataframe(filterbyname)
 
 selected_transmission = sidebar.selectbox("Seleccionar transmicion: ", data ['transmission'].unique())
@@ -61,3 +66,25 @@ if (btnFilterbyTransmission):
     count_row = filterbyTransmission.shape[0]
     st.write(f"Total items : {count_row}")
     st.dataframe(filterbyTransmission)
+
+
+#--- GRAFICAS ---#
+com_precio = df['model']
+if st.sidebar.checkbox('Autos y sus Precios'):
+    fig, ax = plt.subplots()
+    plt.xticks(rotation=90)
+    ax.hist(com_precio, bins=20, range=(0, 20))
+    ax.set_xlabel('Modelo del auto')
+    ax.set_ylabel('Precio')
+    ax.set_title('Grafica que compara el precio de n autos')
+    st.pyplot(fig)
+
+com = st.sidebar.multiselect("Combustible", sorted(data["fuelType"].unique()))
+mod = st.sidebar.multiselect("Modelo auto", sorted(data["model"].unique()))
+
+if st.sidebar.button("Filtrar auto"):
+    st.markdown("Se selecciona el numero de goles de visitante y local y muesta los resultados de  partidos con ese numero de goles")
+    mask = (df["fuelType"].isin(com)) & (df["model"].isin(mod))
+    juegos_seleccionados = df[mask]
+    st.write("Goles Seleccionados:")
+    st.write(juegos_seleccionados)
